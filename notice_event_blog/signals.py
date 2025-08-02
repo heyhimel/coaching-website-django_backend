668c72f979b2e1a66ab49346900046fa366883f6
Kazi Tanvir Ahmed <kazitanvirahmed571@gmail.com>
2025-08-02 12:58:49 +0600

    pdf file remove signals addition

--- notice_event_blog/signals.py
+++ notice_event_blog/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save,post_delete
 from django.dispatch import receiver
-from .models import notice_banner,blog_banner,events_banner,blog_details,events_details,event_speakers
+from .models import notice_banner,blog_banner,events_banner,blog_details,events_details,event_speakers,notice_details
 import os
 
 # banner_about_page banner unused image remove 
@@ -152,3 +152,27 @@ def delete_banner_image_on_delete(sender, instance, **kwargs):
     if instance.speaker_photo:
         if os.path.isfile(instance.speaker_photo.path):
             os.remove(instance.speaker_photo.path)
+
+
+# notice_details unused file remove
+@receiver(pre_save, sender=notice_details)
+def delete_old_notice_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        # New object being created; nothing to delete
+        return
+
+    try:
+        old_file = notice_details.objects.get(pk=instance.pk).notice_file
+    except notice_details.DoesNotExist:
+        return
+
+    new_file = instance.notice_file
+    if old_file and old_file != new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+
+@receiver(post_delete, sender=notice_details)
+def delete_notice_file_on_delete(sender, instance, **kwargs):
+    if instance.notice_file:
+        if os.path.isfile(instance.notice_file.path):
+            os.remove(instance.notice_file.path)
