2363b64d456fadb70808d837a7dad69b3ab8b497
Kazi Tanvir Ahmed <kazitanvirahmed571@gmail.com>
2025-08-03 14:59:49 +0600

    static_root add for nginx, settings.py

--- eduspringaac/settings.py
+++ eduspringaac/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles' # for nginx: to collect all static files here and serve from here.
+# STATICFILES_DIRS = [BASE_DIR / "static"] # for development
+STATIC_ROOT = BASE_DIR / 'staticfiles' # for production nginx: to collect all static files here and serve from here.
 
 # For media file like Image
 MEDIA_ROOT = BASE_DIR / "media/"
